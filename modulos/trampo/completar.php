b8283a010c55141f63308c068f1caf71ec08a265
luxu <zicadopv@gmail.com>
2013-09-16 02:55:46 -0300

    Arquivo do Sistema

--- modulos/trampo/completar.php
+++ modulos/trampo/completar.php
@@ -1,7 +1,7 @@
 <?
 	$hostname = "174.142.48.58";
 	$user = "luxucom";
-	$pass = "L3ux@4BrL#";
+	$pass = "";
 	$basedados = "luxucom_makinson";
 	$connect = mysql_connect($hostname,$user,$pass) or die ("Impossível estabelecer conexao com o servidor de banco de dados");
 	mysql_select_db($basedados)	or die ("Impossivel estabelecer conexão com o banco de dados");
